bbf2e7d7ec05ee14f8c2e8f720cd9dbc176b9cfd
ignidis <patch@cybearth.net>
2019-06-01 20:25:04 +0200

    use multistage build to reduce final image size

--- stable/amd64/centos/build.sh
+++ stable/amd64/centos/build.sh
@@ -6,4 +6,4 @@
 #
 # Must run as superuser, either you are root or must sudo 
 #
-docker build --build-arg NAME="nginx" --build-arg NGINX_VERSION="$1" --build-arg NGINX_REPO="nginx-stable" --build-arg NGINX_SHORT_REPO="stable" --build-arg NGINX_SVC_NAME="nginx" --build-arg NGINX_SVC_UID="9001" -t nginx:"$1"-amd64-centos .
+docker build --build-arg NAME="NGINX" --build-arg NGINX_VERSION="$1" --build-arg NGINX_REPO="nginx-stable" --build-arg NGINX_SHORT_REPO="stable" --build-arg NGINX_SVC_NAME="nginx" --build-arg NGINX_SVC_UID="9001" -t nginx:"$1"-amd64-centos .
